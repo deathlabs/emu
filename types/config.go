3b3170110e1f210e21cddc195b5ca94f6f05efd3
cyberphor <48382964+cyberphor@users.noreply.github.com>
2026-03-28 19:36:45 +0000

    Updated config properties

--- types/config.go
+++ types/config.go
@@ -1,12 +1,26 @@
 package types
 
+type Credential struct {
+	APIKey      string `json:"api_key"`
+	Certifcates struct {
+		PublicKeyPath  string `json:"public_key_path"`
+		PrivateKeyPath string `json:"private_key_path"`
+	} `json:"certificates"`
+}
+
+type System struct {
+	Credential Credential `json:"credential"`
+}
+
 type Config struct {
 	EMASS struct {
-		User struct {
-			UID int `json:"uid"`
-		} `json:"user"`
-		API struct {
-			URL string `json:"url"`
-		} `json:"api"`
+		URL         string                `json:"url"`
+		Credentials map[string]Credential `json:"credentials"`
+		Systems     map[string]System     `json:"systems"`
 	} `json:"emass"`
+	EMU struct {
+		Output struct {
+			Format string `json:"format"`
+		} `json:"output"`
+	} `json:"emu"`
 }
