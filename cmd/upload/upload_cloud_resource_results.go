d5bd9a226324bab1afa2214a0806213183cd8a33
cyberphor <48382964+cyberphor@users.noreply.github.com>
2026-07-30 02:33:09 +0000

    Begin emu upload cloud-resource-results

--- cmd/upload/upload_cloud_resource_results.go
+++ cmd/upload/upload_cloud_resource_results.go
@@ -2,18 +2,75 @@ package upload
 
 import (
 	"fmt"
+	"net/http"
+	"net/url"
 
+	"github.com/deathlabs/emu/v4/config"
+	"github.com/deathlabs/emu/v4/emass"
+	"github.com/deathlabs/emu/v4/models"
+	"github.com/deathlabs/emu/v4/output"
 	"github.com/spf13/cobra"
+)
+
+const (
+	cloudResourceResultsProviderDefault   = ""
+	cloudResourceResultsResourceIdDefault = ""
+)
+
+var (
+	cloudResourceResultsProvider   string
+	cloudResourceResultsResourceId string
 )
 
 var (
 	uploadCloudResourceResultsCmd = &cobra.Command{
 		Use:   "cloud-resource-results",
 		Short: "Upload cloud resource results",
-		Run:   updateCloudResourceResults,
+		RunE:  updateCloudResourceResults,
 	}
 )
 
-func updateCloudResourceResults(cmd *cobra.Command, args []string) {
-	fmt.Println("emu update artifact")
+func updateCloudResourceResults(cmd *cobra.Command, args []string) error {
+	var (
+		endpoint string
+		err      error
+		params   url.Values
+		response *http.Response
+		system   models.System
+		systems  []models.System
+	)
+
+	params = url.Values{}
+
+	if cloudResourceResultsProvider != cloudResourceResultsProviderDefault {
+		if len(cloudResourceResultsProvider) > 100 {
+			return fmt.Errorf("the Provider length cannot exceed %d characters", 100)
+		}
+		params.Set("provider", cloudResourceResultsProvider)
+	}
+
+	systems, err = config.FilterSystems(config.Data, config.ActiveProfileName, config.SystemIDs)
+	if err != nil {
+		return err
+	}
+
+	for _, system = range systems {
+		endpoint = fmt.Sprintf("%s/api/systems/%d/cloud-resource-results", config.Data.URL, system.ID)
+
+		if len(params) > 0 {
+			endpoint = fmt.Sprintf("%s?%s", endpoint, params.Encode())
+		}
+
+		response, err = emass.Post(system.ConfigProfile, endpoint, nil, nil)
+		if err != nil {
+			return err
+		}
+
+		err = output.Response(response, config.OutputFormat)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
